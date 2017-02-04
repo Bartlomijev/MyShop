d449d2bbbf2bbd9c47962477d12d73514847c27c
Mateusz Paprocki <mk.paprocki@gmail.com>
2017-02-04 09:31:11 +0100

    Seting up sql plugin and pom.xml file

---
+++ test/sql/test-data.sql
@@ -0,0 +1,5 @@
+insert into Accounts (USER_NAME, ACTIVE, PASSWORD, USER_ROLE)
+values ('employee1', 1, '123', 'EMPLOYEE');
+
+insert into Accounts (USER_NAME, ACTIVE, PASSWORD, USER_ROLE)
+values ('manager1', 1, '123', 'MANAGER');
